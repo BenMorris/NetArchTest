447c4e2b17294352b3b85cc9c44d3d00cf435d57
Nick Harvey <Nick.Harvey.Developer@gmail.com>
2019-03-14 09:30:53 -0500

    removed unused using

--- src/NetArchTest.Rules/Extensions/TypeDefinitionExtensions.cs
+++ src/NetArchTest.Rules/Extensions/TypeDefinitionExtensions.cs
@@ -4,7 +4,6 @@
     using System.Linq;
     using System.Collections.Generic;
     using Mono.Cecil;
-    using System.Reflection;
 
     /// <summary>
     /// Extensions for the <see cref="TypeDefinition"/> class.
