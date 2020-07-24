a9efeb648fa4c23abc9f8f7d86123e3f20709161
Ben Morris <ben@ben-morris.com>
2020-07-24 18:46:09 +0100

    Added a custom rules implementation that allows NetArchTest to be extended using external rule classes.

---
+++ samples/NetArchTest.SampleRules/CustomRule.cs
@@ -0,0 +1,19 @@
+﻿namespace NetArchTest.SampleRules
+{
+    using Mono.Cecil;
+    using NetArchTest.Rules;
+
+    /// <summary>
+    /// A custom rule that can be used to extend NetArchTest.
+    /// </summary>
+    public class CustomRule : ICustomRule
+    {
+        /// <remarks>
+        /// This method will be executed against every type - so take care over external look-ups, etc
+        /// </remarks>
+        public bool MeetsRule(TypeDefinition type)
+        {
+            return true;
+        }
+    }
+}
