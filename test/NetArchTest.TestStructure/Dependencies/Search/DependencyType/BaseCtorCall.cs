6815f3bb969fd102fbf086515cbe5f3ca6d1fe00
adloma16 <marcus.lorentschk@siemens.com>
2020-10-30 13:24:12 +0100

    fix: consider types referenced by fields inside ctor base calls

---
+++ test/NetArchTest.TestStructure/Dependencies/Search/DependencyType/BaseCtorCall.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NetArchTest.TestStructure.Dependencies.Search.DependencyType
+{
+    public struct Id
+    {
+    }
+
+    public static class StaticType
+    {
+        public static readonly Id SomeId;
+    }
+
+    public abstract class BaseCtorCallBase
+    {
+#pragma warning disable 219
+        protected BaseCtorCallBase(params Id[] ids)
+        {
+        }
+#pragma warning restore 219
+    }
+
+    public class BaseCtorCall : BaseCtorCallBase
+    {
+        public BaseCtorCall() :
+            base(StaticType.SomeId)
+        {
+        }
+    }
+}
