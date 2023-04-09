2a9a318e301ec53eb38eebbeccd23b1163a00e66
meyerhenning <henning@sealedcore.tech>
2023-04-09 17:37:50 +0200

    refactor(PropertyDefinitionExtension): use expression style

--- src/NetArchTest.Rules/Extensions/PropertyDefinitionExtensions.cs
+++ src/NetArchTest.Rules/Extensions/PropertyDefinitionExtensions.cs
@@ -13,18 +13,14 @@ namespace NetArchTest.Rules.Extensions
         /// <param name="propertyDefinition">The property to test.</param>
         /// <returns>An indication of whether the property is readonly.</returns>
         public static bool IsReadonly(this PropertyDefinition propertyDefinition)
-        {
-            return propertyDefinition.SetMethod == null || !propertyDefinition.SetMethod.IsPublic;
-        }
-        
+            => !propertyDefinition.SetMethod?.IsPublic ?? true;
+
         /// <summary>
         /// Tests whether a property is nullable
         /// </summary>
         /// <param name="propertyDefinition">The property to test.</param>
         /// <returns>An indication of whether the property is nullable.</returns>
         public static bool IsNullable(this PropertyDefinition propertyDefinition)
-        {
-            return propertyDefinition.PropertyType.IsNullable();
-        }
+            => propertyDefinition.PropertyType.IsNullable();
     }
 }
