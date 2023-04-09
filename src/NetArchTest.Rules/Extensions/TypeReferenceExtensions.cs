46bff6a493d01431c00e61af3053ea325d47798a
meyerhenning <henning@sealedcore.tech>
2023-04-09 17:38:33 +0200

    refactor(TypeReferenceExtensions): use expression style

--- src/NetArchTest.Rules/Extensions/TypeReferenceExtensions.cs
+++ src/NetArchTest.Rules/Extensions/TypeReferenceExtensions.cs
@@ -10,20 +10,15 @@ namespace NetArchTest.Rules.Extensions
         /// <param name="typeReference">The class to test.</param>
         /// <returns>An indication of whether the type has any memebers that are non-nullable value types</returns>
         public static bool IsNullable(this TypeReference typeReference)
-        {
-            return !typeReference.IsValueType || typeReference.Resolve().ToType() == typeof(System.Nullable<>);
-        }
+            => !typeReference.IsValueType 
+               || typeReference.Resolve().ToType() == typeof(System.Nullable<>);
 
         /// <summary>
         /// Returns namespace of the given type, if the type is nested, namespace of containing type is returned instead
         /// </summary>        
         public static string GetNamespace(this TypeReference typeReference)
-        {
-            if (typeReference.IsNested)
-            {
-                return typeReference?.DeclaringType.FullName;
-            }
-            return typeReference.Namespace;
-        }
+            => typeReference.IsNested
+                ? typeReference.DeclaringType.FullName
+                : typeReference.Namespace;
     }
 }
